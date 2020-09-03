dca388f2c24a5cc26cb1d2a4d1a279ee721d22b6
estea chen <estea8968@gmail.com>
2020-09-03 22:53:42 +0800

    modify default-project

--- src/lib/default-project/index.js
+++ src/lib/default-project/index.js
@@ -6,6 +6,7 @@ import meowWav from '!arraybuffer-loader!./83c36d806dc92327b9e7049a565c6bff.wav'
 import backdrop from '!raw-loader!./cd21514d0531fdffb22204e0ec5ed84a.svg';
 import costume1 from '!raw-loader!./b7853f557e4426412e64bb3da6531a99.svg';
 import costume2 from '!raw-loader!./e6ddc55a6ddd9cc9d84fe0b4c21e016f.svg';
+import pag01 from '!raw-loader!./b81aab0acaa208c7521833d32fda4b1d.svg';
 /* eslint-enable import/no-unresolved */
 
 const defaultProject = translator => {
@@ -49,6 +50,12 @@ const defaultProject = translator => {
         assetType: 'ImageVector',
         dataFormat: 'SVG',
         data: encoder.encode(costume2)
+    },
+    {
+        id: 'b81aab0acaa208c7521833d32fda4b1d',
+        assetType: 'ImageVector',
+        dataFormat: 'SVG',
+        data: encoder.encode(pag01)
     }];
 };
 
