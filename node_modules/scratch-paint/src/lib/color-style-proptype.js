167ac6532ae102b35ac82362bb62ce335e98d61d
estea chen <estea8968@gmail.com>
2020-10-14 08:51:18 +0800

    update node_modules warning

--- node_modules/scratch-paint/src/lib/color-style-proptype.js
+++
@@ -1,9 +0,0 @@
-import {PropTypes} from 'prop-types';
-
-import GradientTypes from './gradient-types';
-
-export default PropTypes.shape({
-    primary: PropTypes.string,
-    secondary: PropTypes.string,
-    gradientType: PropTypes.oneOf(Object.keys(GradientTypes)).isRequired
-});
